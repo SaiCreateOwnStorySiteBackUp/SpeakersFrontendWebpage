6df6c2f7a22850fe6eec2c214b392a1f2bbf5feb
pavan <seshasaim72@gmail.com>
2025-06-17 11:06:00 +0530

    committing changes autoscreenSize about,contact and contact email triggering

--- public/js/config.js
+++ public/js/config.js
@@ -1,3 +1,5 @@
 // const BACKEND_BASE_URL = "https://speakersbackendwebpage.onrender.com";
-window.BACKEND_BASE_URL = "https://speakersbackendwebpage.onrender.com";
 // const backendUrl = window.BACKEND_BASE_URL;
+
+window.BACKEND_BASE_URL = "https://speakersbackendwebpage.onrender.com";
+// window.BACKEND_BASE_URL = "http://localhost:5000";
